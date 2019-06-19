508ea878ee28d8f42aa12224f4072c7d99f2217a
Jonathan Boos <gameguy37@gmail.com>
2019-06-19 13:27:02 -0400

    fixed re-render on refresh diagram

--- src/index.js
+++ src/index.js
@@ -111,216 +111,218 @@ document.addEventListener('DOMContentLoaded', () => {
         const game = new Game(npcSpeedMultiplierBool, angledEnemySpawnsBool, playerSpeedMultiplierBool, enemySpawnFrequencyMultiplierBool, enemySpeedRandomBool);
         new GameView(game, ctx).start();
         startGame.blur();
-
     }
+
+    function renderDiagram() {
+        const canvasDiagram = document.getElementById('diagram');
+        canvasDiagram.width = 200;
+        canvasDiagram.height = 600;
+        const ctxDiagram = canvasDiagram.getContext('2d');
+
+        ctxDiagram.clearRect(0, 0, 200, 600);
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.beginPath();
+        ctxDiagram.fillRect(0, 0, 200, 600);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "20px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "rgba(204, 204, 204, 1)";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.fillText("ENEMIES", 100, 25);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#f45942";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#f45942";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.arc(100, 75, 20, 0, 2 * Math.PI, true);
+        ctxDiagram.stroke();
+        ctxDiagram.fill();
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#a442f4";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#a442f4";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.strokeRect(60, 125, 80, 10);
+        ctxDiagram.fillRect(60, 125, 80, 10);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#f9e43e";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#f9e43e";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.strokeRect(70, 170, 60, 30);
+        ctxDiagram.fillRect(70, 170, 60, 30);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#fca420";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#fca420";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.strokeRect(85, 235, 30, 60);
+        ctxDiagram.fillRect(85, 235, 30, 60);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#f442e8";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#f442e8";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.strokeRect(85, 330, 30, 30);
+        ctxDiagram.fillRect(85, 330, 30, 30);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "20px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "rgba(204, 204, 204, 1)";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.shadowBlur = 0;
+        ctxDiagram.fillText("POWERUPS", 100, 405);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#48f442";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#48f442";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.arc(35, 450, 12, 0, 2 * Math.PI, true);
+        ctxDiagram.stroke();
+        ctxDiagram.fill();
+        ctxDiagram.closePath();
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "20px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "white";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.fillText('S', 35, 453);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "16px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.shadowBlur = 0;
+        ctxDiagram.fillText("SLOW TIME", 125, 450);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#48f442";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#48f442";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.arc(35, 485, 12, 0, 2 * Math.PI, true);
+        ctxDiagram.stroke();
+        ctxDiagram.fill();
+        ctxDiagram.closePath();
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "20px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "white";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.fillText('!', 35, 488);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "16px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.shadowBlur = 0;
+        ctxDiagram.fillText("INVINCIBLE", 125, 485);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#48f442";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#48f442";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.arc(35, 520, 12, 0, 2 * Math.PI, true);
+        ctxDiagram.stroke();
+        ctxDiagram.fill();
+        ctxDiagram.closePath();
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "24px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "white";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.fillText('+', 35, 521);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "16px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.shadowBlur = 0;
+        ctxDiagram.fillText("SCORE PLUS", 125, 520);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.fillStyle = "#000000";
+        ctxDiagram.strokeStyle = "#48f442";
+        ctxDiagram.lineWidth = 5;
+        ctxDiagram.shadowColor = "#48f442";
+        ctxDiagram.shadowBlur = 30;
+        ctxDiagram.shadowOffsetX = 0;
+        ctxDiagram.shadowOffsetY = 0;
+        ctxDiagram.arc(35, 555, 12, 0, 2 * Math.PI, true);
+        ctxDiagram.stroke();
+        ctxDiagram.fill();
+        ctxDiagram.closePath();
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "20px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "white";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.fillText('X', 35, 558);
+        ctxDiagram.closePath();
+
+        ctxDiagram.beginPath();
+        ctxDiagram.font = "16px Saira Semi Condensed";
+        ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
+        ctxDiagram.textAlign = "center";
+        ctxDiagram.textBaseline = "middle";
+        ctxDiagram.shadowBlur = 0;
+        ctxDiagram.fillText("CLEAR SCREEN", 125, 555);
+        ctxDiagram.closePath();
+    }
+
+    document.fonts.load('10pt "Saira Semi Condensed"').then(renderDiagram);
+
 });
 
-function renderDiagram() {
-    const canvasDiagram = document.getElementById('diagram');
-    canvasDiagram.width = 200;
-    canvasDiagram.height = 600;
-    const ctxDiagram = canvasDiagram.getContext('2d');
 
-    ctxDiagram.clearRect(0, 0, 200, 600);
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.beginPath();
-    ctxDiagram.fillRect(0, 0, 200, 600);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "20px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "rgba(204, 204, 204, 1)";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.fillText("ENEMIES", 100, 25);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#f45942";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#f45942";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.arc(100, 75, 20, 0, 2 * Math.PI, true);
-    ctxDiagram.stroke();
-    ctxDiagram.fill();
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#a442f4";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#a442f4";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.strokeRect(60, 125, 80, 10);
-    ctxDiagram.fillRect(60, 125, 80, 10);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#f9e43e";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#f9e43e";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.strokeRect(70, 170, 60, 30);
-    ctxDiagram.fillRect(70, 170, 60, 30);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#fca420";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#fca420";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.strokeRect(85, 235, 30, 60);
-    ctxDiagram.fillRect(85, 235, 30, 60);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#f442e8";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#f442e8";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.strokeRect(85, 330, 30, 30);
-    ctxDiagram.fillRect(85, 330, 30, 30);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "20px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "rgba(204, 204, 204, 1)";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.shadowBlur = 0;
-    ctxDiagram.fillText("POWERUPS", 100, 405);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#48f442";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#48f442";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.arc(35, 450, 12, 0, 2 * Math.PI, true);
-    ctxDiagram.stroke();
-    ctxDiagram.fill();
-    ctxDiagram.closePath();
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "20px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "white";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.fillText('S', 35, 453);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "16px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.shadowBlur = 0;
-    ctxDiagram.fillText("SLOW TIME", 125, 450);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#48f442";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#48f442";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.arc(35, 485, 12, 0, 2 * Math.PI, true);
-    ctxDiagram.stroke();
-    ctxDiagram.fill();
-    ctxDiagram.closePath();
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "20px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "white";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.fillText('!', 35, 488);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "16px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.shadowBlur = 0;
-    ctxDiagram.fillText("INVINCIBLE", 125, 485);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#48f442";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#48f442";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.arc(35, 520, 12, 0, 2 * Math.PI, true);
-    ctxDiagram.stroke();
-    ctxDiagram.fill();
-    ctxDiagram.closePath();
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "24px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "white";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.fillText('+', 35, 521);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "16px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.shadowBlur = 0;
-    ctxDiagram.fillText("SCORE PLUS", 125, 520);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.fillStyle = "#000000";
-    ctxDiagram.strokeStyle = "#48f442";
-    ctxDiagram.lineWidth = 5;
-    ctxDiagram.shadowColor = "#48f442";
-    ctxDiagram.shadowBlur = 30;
-    ctxDiagram.shadowOffsetX = 0;
-    ctxDiagram.shadowOffsetY = 0;
-    ctxDiagram.arc(35, 555, 12, 0, 2 * Math.PI, true);
-    ctxDiagram.stroke();
-    ctxDiagram.fill();
-    ctxDiagram.closePath();
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "20px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "white";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.fillText('X', 35, 558);
-    ctxDiagram.closePath();
-
-    ctxDiagram.beginPath();
-    ctxDiagram.font = "16px Saira Semi Condensed";
-    ctxDiagram.fillStyle = "rgba(204, 204, 204, 0.75)";
-    ctxDiagram.textAlign = "center";
-    ctxDiagram.textBaseline = "middle";
-    ctxDiagram.shadowBlur = 0;
-    ctxDiagram.fillText("CLEAR SCREEN", 125, 555);
-    ctxDiagram.closePath();
-}
-
-document.fonts.load('10pt "Saira Semi Condensed"').then(renderDiagram);
 
